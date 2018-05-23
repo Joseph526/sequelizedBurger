2a9b84518965920c9a0b1bafce005fd73db9bb32
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-23 00:34:48 -0400

    Refactoring routes with Sequelize queries

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,42 @@
-var express = require("express");
+// var express = require("express");
 
-var router = express.Router();
+// var router = express.Router();
 
 // Import the model
-var burger = require("../models/burger.js");
+var db = require("../models");
 
-// Declare the routes
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-        var hbsObject = {
-            burgers: data
-        };
-        res.render("index", hbsObject);
+module.exports = function(app) {
+    // Declare the routes
+    app.get("/", function(req, res) {
+        db.Burger.findAll({}).then(function(data) {
+            var hbsObject = {
+                burgers: data
+            };
+            res.render("index", hbsObject);
+        });
     });
-});
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function(result) {
-        res.json({ id: result.insertId });
+    app.post("/api/burgers", function(req, res) {
+        burger.insertOne([
+            "burger_name", "devoured"
+        ], [
+                req.body.burger_name, req.body.devoured
+            ], function(result) {
+                res.json({ id: result.insertId });
+            });
     });
-});
 
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    burger.updateOne({
-        devoured: true
-    }, condition, function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        }
-        else {
-            res.status(200).end();
-        }
+    app.put("/api/burgers/:id", function(req, res) {
+        var condition = "id = " + req.params.id;
+        burger.updateOne({
+            devoured: true
+        }, condition, function(result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end();
+            }
+        });
     });
-});
-
-// Export the routes
-module.exports = router;
+};
