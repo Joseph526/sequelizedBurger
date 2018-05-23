74b0b7cdcfe2b173a7cb4e4d5e5794f79983df9f
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-22 23:54:25 -0400

    Created models

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
-// Import the ORM
-var orm = require("../config/orm.js");
+module.exports = function(sequelize, Sequelize) {
+    var Burger = sequelize.define("Burger", {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        burger_name: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+        devoured: {
+            type: Sequelize.BOOLEAN
+        }
+    });
 
-var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
-            cb(res);
-        });
-    },
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res);
-        });
-    },
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
+    return Burger;
 };
-
-// Export the database functions for the controller
-module.exports = burger;
